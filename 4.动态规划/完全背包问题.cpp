883d909c3c847a17daf3c2c55e9c8a74cae99980
ltw <3245849061@qq.com>
2020-04-22 21:05:51 +0800

    完全背包问题

---
+++ 4.动态规划/完全背包问题.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int all, n, w[105], v[105], ans[105][10005];
+
+int main() {
+	cin >> n >> all;
+	for (int i = 1; i <= n; i++) {
+		cin >> w[i] >> v[i];
+	} 
+	for (int i = 1; i<= n; i++) {
+		for (int j = 1; j <= all; j++) {
+			if (j < w[i]) {
+				ans[i][j] = ans[i - 1][j];
+			} else {
+				ans[i][j] = max(ans[i - 1][j], v[i] + ans[i][j - w[i]]);
+			}
+		}
+	}
+	cout << ans[n][all] << endl;
+	return 0;
+}
