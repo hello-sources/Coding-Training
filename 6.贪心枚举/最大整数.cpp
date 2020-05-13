97d6591c0a0988eef0e8aaaeaa5000a5b6a9b13f
ltw <3245849061@qq.com>
2020-05-13 19:02:08 +0800

    贪心最大整数问题

---
+++ 6.贪心枚举/最大整数.cpp
@@ -0,0 +1,32 @@
+/*************************************************************************
+    > File Name: 最大整数.cpp
+    > Author: ltw
+    > Mail: 3245849061@qq.com 
+    > Created Time: Wed 13 May 2020 07:00:29 PM CST
+ ************************************************************************/
+
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int n;
+string str[100005];
+
+bool cmp(string a, string b) {
+	return a + b > b + a; 
+}
+
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> str[i];
+	}
+	sort(str, str + n, cmp);
+	for (int i = 0; i < n; i++) {
+		cout << str[i];
+	}
+	cout << endl;
+	return 0;
+} 
