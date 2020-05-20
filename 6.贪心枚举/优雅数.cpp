6f4014a03bc60b21e4ae2bc378aa7a79d55753de
ltw <3245849061@qq.com>
2020-05-20 21:04:46 +0800

    优雅数

--- 6.贪心枚举/优雅数.cpp
+++ 6.贪心枚举/优雅数.cpp
@@ -12,22 +12,23 @@ long long a, b, ans;
 
 int main() {
 	cin >> a >> b;
-	for (int i = 0; i <= 9; i++) {
-		for (int j = 0; j <= 9; j++) {
+	for (int i = 0; i <= 9; i++) {//一堆数
+		for (int j = 0; j <= 9; j++) {//一个数
 			if (i == j) continue;
-			for (int k = 3; k <= 17; k++) {
-				for (int l = 1; l <= k; l++) {
-					if (j == 0 && l == 1) continue;
-					if (i == 0 && l != 1) break;
+			for (int k = 3; k <= 17; k++) {//数的总长度
+				for (int l = 1; l <= k; l++) {//一个数的位置
+					if (j == 0 && l == 1) continue;//前置零的情况
+					if (i == 0 && l != 1) break;//都continue也行
 					long long t = 0;
 					for (int m = 1; m <= k; m++) {
+						//构造一个优雅数
 						if (m != l) {
 							t = t * 10 + i;
 						} else {
 							t = t * 10 + j;
 						}
 					}
-					if (t >= a && t <= b) ans++;
+					if (t >= a && t <= b) ans++;//找出对应范围内的数
 				}
 			}
 		}
