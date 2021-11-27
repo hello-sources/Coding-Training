ba06c9e96149c96e2ad3a81d83e30c840db06879
hello-sources <3245849061@qq.com>
2021-11-27 18:12:10 +0800

    前缀统计

---
+++ LeetCode/HZOJ281前缀统计.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+#define MAX_N 1000000
+
+struct Node {
+    int flag;
+    int next[26];
+} tree[MAX_N + 5];
+int root = 1, cnt = 1;
+char str[MAX_N + 5];
+
+int getNode() { return ++cnt; }
+void insert(const char *str) {
+    int p = root;
+    for (int i = 0; str[i]; i++) {
+        int ind = str[i] - 'a';
+        if (tree[p].next[ind] == 0) tree[p].next[ind] = getNode();
+        p = tree[p].next[ind];
+    }
+    tree[p].flag += 1;
+    return ;
+}
+
+int query(const char *str) {
+    int p = root, cnt = 0;
+    for (int i = 0; str[i] && p; i++) {
+        p = tree[p].next[str[i] - 'a'];
+        cnt += tree[p].flag;
+    }
+    return cnt;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    while (n--) cin >> str, insert(str);
+    while (m--) cin >> str, cout << query(str) << endl;
+    return 0;
+}
