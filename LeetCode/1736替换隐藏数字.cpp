9f3c86dc042ab64835491c542fb54dbb59455198
hello-sources <3245849061@qq.com>
2021-12-05 17:51:12 +0800

    1736替换隐藏数字

---
+++ LeetCode/1736替换隐藏数字.cpp
@@ -0,0 +1,15 @@
+char * maximumTime(char * time){
+    if (time[0] == '?') {
+        time[0] = (time[1] <= '9' && time[1] >= '4') ? '1' : '2';
+    }
+    if (time[1] == '?') {
+        time[1] = (time[0] == '2') ? '3' : '9';
+    }
+    if (time[3] == '?') {
+        time[3] = '5';
+    }
+    if (time[4] == '?') {
+        time[4] = '9';
+    }
+    return time;
+}
