9244b5e6930c1b5a26ba5453ee1e9ae7539fbac4
hello-sources <3245849061@qq.com>
2021-12-17 17:30:14 +0800

    1518换酒问题

---
+++ LeetCode/1518换酒问题.cpp
@@ -0,0 +1,8 @@
+int numWaterBottles(int numBottles, int numExchange){
+    int ans = numBottles;
+    while (numBottles >= numExchange) {
+        ans += numBottles / numExchange;
+        numBottles = numBottles / numExchange + numBottles % numExchange;
+    }
+    return ans;
+}
