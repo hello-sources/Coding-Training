3c8aa0757122f84c7374f8599ba6f93986546bd7
hello-sources <3245849061@qq.com>
2021-12-25 10:27:14 +0800

    剑指offer4二维数组中的查找

---
+++ LeetCode/剑指offer4二维数组中的查找.cpp
@@ -0,0 +1,10 @@
+bool findNumberIn2DArray(int** matrix, int matrixSize, int* matrixColSize, int target){
+    if (matrixSize == 0 || matrixColSize[0] == 0) return false;
+    int i = 0, j = matrixColSize[0] - 1;
+    while (i < matrixSize && j >= 0) {
+        if (matrix[i][j] == target) return true;
+        else if (matrix[i][j] < target) i++;
+        else j--;
+    }
+    return false;
+}
