efc51547cb5f3ce0d4b18faa69e27271aebda6ce
hello-sources <3245849061@qq.com>
2022-01-01 11:38:17 +0800

    206反转链表

---
+++ LeetCode/206反转链表.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* reverseList(struct ListNode* head){
+    struct ListNode *p, *q, ret;
+    if (head == NULL) return head;
+    p = head;
+    ret.next = NULL;
+    while (p) {
+        q = p->next;
+        p->next = ret.next;
+        ret.next = p;
+        p = q;
+    }
+    return ret.next;
+}
