d02b5a030cec5c44ea356275b0c4414a655381c6
hello-sources <3245849061@qq.com>
2022-01-02 09:19:25 +0800

    46全排列

---
+++ LeetCode/46全排列.cpp
@@ -0,0 +1,34 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+ int count;
+void dfs(int* nums,int numsSize,int depth,int* cur,bool* used,int** res){
+    if(depth == numsSize){
+        res[count] = (int *)malloc(numsSize * sizeof(int));
+        memcpy(res[count++], cur, numsSize * sizeof(int));
+        return;
+    }
+    for(int i = 0; i < numsSize; ++i){
+        if(used[i] == true) continue;
+        cur[depth] = nums[i];
+        used[i] = true;
+        dfs(nums, numsSize, depth+1, cur, used, res);
+        used[i] = false;
+    }
+}
+
+int** permute(int* nums, int numsSize, int* returnSize, int** returnColumnSizes){
+    *returnSize = 1;
+    for(int i=1;i <= numsSize;++i) *returnSize *= i;
+    int** res=(int **)malloc(1000 * sizeof(int *));
+    bool* used=(bool *)malloc(numsSize * sizeof(bool));
+    memset(used, 0, numsSize * sizeof(bool));
+    int* cur=(int *)malloc(numsSize * sizeof(int));
+    count = 0;
+    dfs(nums, numsSize, 0, cur, used, res);
+    *returnColumnSizes=(int *)malloc((*returnSize) * sizeof(int));
+    for(int i = 0; i < *returnSize; ++i) (*returnColumnSizes)[i] = numsSize;
+    return res;
+}
