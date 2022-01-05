47f6eb9a845e7247a382aad10f6803304867b86d
hello-sources <3245849061@qq.com>
2022-01-05 15:12:36 +0800

    53最大子数组和

---
+++ LeetCode/53最大子数组和.cpp
@@ -0,0 +1,9 @@
+
+int maxSubArray(int* nums, int numsSize){
+    int temp = 0, max = nums[0];
+    for (int i = 0; i < numsSize; i++) {
+        temp = fmax(temp + nums[i], nums[i]);
+        max = fmax(max, temp);
+    }
+    return max;
+}
