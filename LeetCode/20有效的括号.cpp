ebb1d2527b15405809d890cfa7325d2338b9fd1c
hello-sources <3245849061@qq.com>
2022-01-06 18:48:51 +0800

    20有效的括号

---
+++ LeetCode/20有效的括号.cpp
@@ -0,0 +1,20 @@
+//用数组模拟栈
+bool isValid(char *s) {
+    int len = strlen(s);
+    char *stack = (char *)malloc(sizeof(char) * len);
+    int top = -1, flag = 1;//用flag标记
+    while (s[0]) {
+        switch (s[0]) {
+            case '(':
+            case '[':
+            case '{': stack[++top] = s[0]; break;
+            case ')': flag = (top != -1 && stack[top--] == '('); break;
+            case ']': flag = (top != -1 && stack[top--] == '['); break;
+            case '}': flag = (top != -1 && stack[top--] == '{'); break;
+        }
+        if (!flag) break;
+        s++;
+    }
+    free(stack);
+    return (flag && top == -1);
+}
