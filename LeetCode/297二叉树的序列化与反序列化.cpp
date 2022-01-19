fdc70e82d646b37c79c941360d96315848ca8eea
hello-sources <3245849061@qq.com>
2022-01-19 09:09:31 +0800

    297二叉树的序列化与反序列化

---
+++ LeetCode/297二叉树的序列化与反序列化.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/** Encodes a tree to a single string. */
+void preorder(struct TreeNode *root, char *res) {
+    if (root == NULL) {
+        strcat(res, "#");
+        strcat(res, ",");
+        return ;
+    }
+    char temp[10] = "";
+    sprintf(temp, "%d", root->val);
+    strcat(res, temp);
+    strcat(res, ",");
+    preorder(root->left, res);
+    preorder(root->right, res);
+    return ;
+}
+
+char* serialize(struct TreeNode* root) {
+    char *res = (char *)malloc(sizeof(char) * 50000);
+    memset(res, 0, sizeof(res));
+    preorder(root, res);
+    return res;
+}
+
+struct TreeNode *build(char *data, int *ind) {
+    if (data[*ind] == '#') {
+        (*ind) += 2;
+        return NULL;
+    }
+    struct TreeNode *root = (struct TreeNode *)malloc(sizeof(struct TreeNode));
+    char temp[10] = "";
+    for (int i = 0; data[*ind] != ','; i++, ++(*ind)) {
+        temp[i] = data[*ind];
+    }
+    ++(*ind);
+    root->val = atoi(temp);
+    root->left = build(data, ind);
+    root->right = build(data, ind);
+    return root;
+}
+
+/** Decodes your encoded data to tree. */
+struct TreeNode* deserialize(char* data) {
+    int ind = 0;
+    return build(data, &ind);
+}
+
+// Your functions will be called as such:
+// char* data = serialize(root);
+// deserialize(data);
