bc3c57bd0bbfb41877ff8ba080fee968c75ce823
hello-sources <3245849061@qq.com>
2022-02-21 18:09:07 +0800

    783二叉搜索树节点最小距离

---
+++ LeetCode/783二叉搜索树节点最小距离.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+void order(struct TreeNode *root, int *num, int *size) {
+    if (root == NULL) return ;
+    order(root->left, num, size);
+    num[(*size)++] = root->val;
+    order(root->right, num, size);
+    return ;
+}
+
+int cmp(const void *a, const void *b) {
+    return *(int *)a - *(int *)b;
+}
+
+int minDiffInBST(struct TreeNode* root){
+    int *res = (int *)malloc(sizeof(int) * 10005);
+    int size = 0;
+    order(root, res, &size);
+    qsort(res, size, sizeof(int), cmp);
+    int min = INT_MAX;
+    for (int i = 1; i < size; i++) {
+        min = fmin(min, res[i] - res[i - 1]);
+    }
+    free(res);
+    return min;
+}
