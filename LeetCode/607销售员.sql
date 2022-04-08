d85e546edbe45989bc674936059100ae5e4142f8
hello-sources <3245849061@qq.com>
2022-04-08 09:03:04 +0800

    607销售员-sql

---
+++ LeetCode/607销售员.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select name from SalesPerson
+where sales_id not in (
+    select Orders.sales_id from Orders
+    left join Company on Company.com_id = Orders.com_id
+    where Company.name = 'RED'
+);
