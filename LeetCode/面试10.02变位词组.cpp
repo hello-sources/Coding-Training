50231c11519256de8616693b58f3c197d576841e
hello-sources <3245849061@qq.com>
2022-04-15 08:23:15 +0800

    面试10.02变位词组.cpp

---
+++ LeetCode/面试10.02变位词组.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> u_map;
+        for (auto &x : strs) {
+            string str = x;
+            sort(str.begin(), str.end());
+            u_map[str].emplace_back(x);
+        }
+        vector<vector<string>> res;
+        for (auto &x : u_map) {
+            res.emplace_back(x.second);
+        }
+        return res;
+    }
+};
