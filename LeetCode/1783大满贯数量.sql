65dfc50607bab939f90b11125c2b0351fa87be9d
hello-sources <3245849061@qq.com>
2022-04-21 08:57:01 +0800

    1783大满贯数量.sql

---
+++ LeetCode/1783大满贯数量.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+select player_id, player_name, sum(
+    (case when Wimbledon = player_id then 1 else 0 end) + 
+    (case when Fr_open = player_id then 1 else 0 end) +
+    (case when US_open = player_id then 1 else 0 end) +
+    (case when Au_open = player_id then 1 else 0 end)
+) grand_slams_count
+from Players p
+inner join Championships c
+group by player_id
+having grand_slams_count > 0
+order by player_id;
