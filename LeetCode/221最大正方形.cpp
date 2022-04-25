253a05bf039b4428e59041f6953a5e62a9f0a7c0
hello-sources <3245849061@qq.com>
2022-04-25 08:15:44 +0800

    modify-221最大正方形

--- LeetCode/221最大正方形.cpp
+++ LeetCode/221最大正方形.cpp
@@ -43,3 +43,24 @@ int maximalSquare(char** matrix, int matrixSize, int* matrixColSize){
     }
     return maxSide * maxSide;
 }
+
+/**********************C++******************/
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        if(m == 0 || n == 0) return 0;
+        vector<vector<int>> dp(m, vector<int>(n));
+        int maxlen = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) dp[i][j] = 1;
+                    else dp[i][j] = min(min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    maxlen = max(maxlen, dp[i][j]);
+                }
+            }
+        }
+        return maxlen * maxlen;
+    }
+};
