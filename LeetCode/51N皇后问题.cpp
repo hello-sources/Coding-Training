6418d2c8fe9b58ded12606d2383b89e6ac8061db
hello-sources <3245849061@qq.com>
2022-04-30 08:30:54 +0800

    modify-N皇后问题

--- LeetCode/51N皇后问题.cpp
+++ LeetCode/51N皇后问题.cpp
@@ -42,3 +42,44 @@ char *** solveNQueens(int n, int* returnSize, int** returnColumnSizes){
     }
     return res;
 }
+
+/***********************回溯C++***************/
+class Solution {
+public:
+    bool judge(int row, int col, vector<string> &chessboard, int n) {
+        int cnt = 0;
+        for (int i = 0; i < row; i++) {
+            if (chessboard[i][col] == 'Q') return false;
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (chessboard[i][j] == 'Q') return false;
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (chessboard[i][j] == 'Q') return false;
+        }
+        return true;
+    }
+
+    void backtracking(int n, int row, vector<string> &chessboard) {
+        if (n == row) {
+            ans.emplace_back(chessboard);
+            return ;
+        }
+        for (int col = 0; col < n; col++) {
+            if (!judge(row, col, chessboard, n)) continue;
+            chessboard[row][col] = 'Q';
+            backtracking(n, row + 1, chessboard);
+            chessboard[row][col] = '.';
+        }
+        return ;
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> chessboard(n, string(n, '.'));
+        backtracking(n, 0, chessboard);
+        return ans;
+    }
+
+private :
+    vector<vector<string>> ans;
+};
