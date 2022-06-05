39242e382c448ac9bd1f5cbc9e0739807bc1b0ec
hello-sources <3245849061@qq.com>
2022-06-05 17:39:05 +0800

    modify-929独特的电子邮件地址

--- LeetCode/929独特的电子邮件地址.cpp
+++ LeetCode/929独特的电子邮件地址.cpp
@@ -31,3 +31,24 @@ int numUniqueEmails(char ** emails, int emailsSize){
     }
     return emailsSize - cnt;
 }
+
+/********************独特的电子邮件地址******************/
+class Solution {
+public:
+    int numUniqueEmails(vector<string> &emails) {
+        unordered_set<string> emailSet;
+        for (auto &email: emails) {
+            string local;
+            for (char c: email) {
+                if (c == '+' || c == '@') {
+                    break;
+                }
+                if (c != '.') {
+                    local += c;
+                }
+            }
+            emailSet.emplace(local + email.substr(email.find('@')));
+        }
+        return emailSet.size();
+    }
+};
