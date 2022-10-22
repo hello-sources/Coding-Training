cb9a1f7af6b7e30529571ed1d72a3e57f677f517
litianwei <3245849061@qq.com>
2022-10-22 09:44:54 +0800

    904水果成篮.java

--- LeetCode/904水果成篮.java
+++ LeetCode/904水果成篮.java
@@ -1,3 +1,9 @@
+/**
+ * 1、Java进行声明HashMap的时候最好使用包装类
+ * 2、给map里面的数组添加数据的时候使用到map.put而不是直接用等号获取
+ * 3、获取数据有两个，一个是get(), 一个是getOrDefault表示如果没有就给个默认值
+ * 4、删除就是remove()
+ */
 class Solution {
     public int totalFruit(int[] fruits) {
         Map<Integer, Integer> map = new HashMap<>();
