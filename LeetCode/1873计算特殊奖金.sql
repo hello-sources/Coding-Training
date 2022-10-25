9819799ee0ded282c9669a43de5a3b33d8b7240a
litianwei <3245849061@qq.com>
2022-10-25 09:23:44 +0800

    leetcode-sql

--- LeetCode/1873计算特殊奖金.sql
+++ LeetCode/1873计算特殊奖金.sql
@@ -3,3 +3,7 @@ select employee_id, CASE WHEN
     MOD(employee_id, 2) = 1 AND name not rlike '^M' THEN salary ELSE 0 END AS bonus
     from employees
      order by employee_id;
+
+# Write your MySQL query statement below
+select employee_id, if (employee_id % 2 = 1 and left(name, 1) != 'M', salary, 0) as bonus
+from Employees order by employee_id
--- LeetCode/595大的国家.sql
+++ LeetCode/595大的国家.sql
@@ -1,2 +1,17 @@
 # Write your MySQL query statement below
 select name, population, area from World where area >= 3000000 or population >= 25000000;
+
+# 通过union连接两个查询，加快查询速度
+SELECT
+    name, population, area
+FROM
+    world
+WHERE
+    area >= 3000000
+UNION
+SELECT
+    name, population, area
+FROM
+    world
+WHERE
+    population >= 25000000
