733f34d35ca6cf47e6d4192a79431a3bf2c955a7
litianwei <3245849061@qq.com>
2022-11-02 10:33:58 +0800

    leetcode-MySQL

--- LeetCode/1084销售分析Ⅲ.sql
+++ LeetCode/1084销售分析Ⅲ.sql
@@ -5,3 +5,8 @@ on s.product_id = p.product_id
 group by s.product_id
 having min(s.sale_date) >= '2019-01-01'
 and max(s.sale_date) <= '2019-03-31';
+
+# where 和 having 区别
+# where 对查询数据进行过滤，
+# having 用于对已分组的数据进行过滤 having 和 group by 必须配合使用
+# 区别：where是数据从磁盘读入内存的时候进行判断，而having是磁盘读入内存后再判断。
